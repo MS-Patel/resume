ff566cdaf4cbc9dcc60c7f708c0091ff23f7234a
MS-Patel <patelmilind1620@gmail.com>
2021-09-14 13:17:31 +0530

    templates updated

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 
+from os import name
 from django.urls import path
+from app import views
 
 urlpatterns = [
-    #path('', ),
-
+    path("", views.login, name="login"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("index", views.index, name="index"),
+    path("create-resume", views.create_resume, name="create-resume"),
+    path("resume", views.resume, name="resume"),
 
 ]
